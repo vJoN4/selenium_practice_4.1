d0d919d00705c4073b24a0b8525fdf041e66cc9a
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2022-09-25 22:56:17 -0500

    Added pyunit report generator

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
+import HtmlTestRunner
+import unittest
 import time
 
-_Driver = webdriver.Edge("./msedgedriver.exe")
+# ! NOTE: Unittest derives from PyUnit
+class DragAndDrop(unittest.TestCase):
 
-_Driver.get('http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
+  def setUp(self):
+    self.nObjects = 7
+    self._Driver = webdriver.Edge("./msedgedriver.exe")
+    self._Driver.maximize_window()
+    self._Driver.get('http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
 
-nObjects = 7
+  def test_drag_and_drop(self):
+    actions = ActionChains(self._Driver)
 
-time.sleep(3)
+    for i in range(1, self.nObjects + 1):
+      card = self._Driver.find_element(By.ID, f'box{i}')
+      box = self._Driver.find_element(By.ID, f'box10{i}')
+      actions.drag_and_drop(card, box).perform()
 
-actions = ActionChains(_Driver)
+    time.sleep(2)
 
-for i in range(1, nObjects + 1):
-  card = _Driver.find_element(By.ID, f'box{i}')
-  box = _Driver.find_element(By.ID, f'box10{i}')
-  actions.drag_and_drop(card, box).perform()
+    self._Driver.quit()
 
-time.sleep(2)
-
-_Driver.quit()
+if __name__ == '__main__':
+  unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='./reports'))
